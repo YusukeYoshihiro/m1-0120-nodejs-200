ce8c9093d32129e5889d552ab591140eb69ac53c
Francois Polo <andasan@gmail.com>
2020-09-08 12:38:11 -0700

    product list view

--- controllers/shop.controller.js
+++ controllers/shop.controller.js
@@ -4,6 +4,11 @@ const Product = require('../models/product.model');
 exports.getProducts = (req,res,next)=>{
     //fetchAll
     Product.fetchAll(products => {
-        res.json(products);
+        // res.json(products);
+        res.render('shops/product-list', {
+            pageTitle: 'All Products',
+            products: products,
+
+        });
     });
 };
